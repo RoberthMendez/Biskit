9b7cbb9dde92a2e467a37a813ae84ef6803b0a9b
luzasalazar <luzasalazar@javeriana.edu.co>
2026-02-27 00:30:36 -0500

    Eliminar mascota

--- biskit/src/main/java/com/example/biskit/controller/VetsController.java
+++ biskit/src/main/java/com/example/biskit/controller/VetsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,6 +36,12 @@ public class VetsController {
   public String mostrarMascota(@PathVariable("id") Integer id, Model model) {
     model.addAttribute("pet", petsService.getPetById(id));
     return "info-pet";
+  }
+
+  @PostMapping("/pets/delete/{id}")
+  public String eliminarMascota(@PathVariable("id") Integer id) {
+    petsService.deletePet(id);
+    return "redirect:/vet/pets";
   }
 
 }
